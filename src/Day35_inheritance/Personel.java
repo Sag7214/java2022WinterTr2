34ad7df6e7892b054951ce5dba819e318f42e100
sag7214 <sagyahzac@gmail.com>
2022-04-06 13:18:44 +0200

    day 36 aktar

---
+++ src/Day35_inheritance/Personel.java
@@ -0,0 +1,9 @@
+package Day35_inheritance;
+
+public class Personel {
+
+    public String isim;
+    protected String soyisim;
+    protected String adres;
+
+}
---
+++ src/Day36_inheritance/Parent.java
@@ -0,0 +1,11 @@
+package Day36_inheritance;
+
+public class Parent {
+
+
+    Parent(){
+        System.out.println("Parent cons. calisti");
+    }
+    protected int sayi;
+
+}
