f0daeee90d61d63fcfd679d920681c49dbecdb5c
sag7214 <sagyahzac@gmail.com>
2022-09-09 16:10:06 +0200

    yeni clas

---
+++ src/Practic_clas/GitHubGonder.java
@@ -0,0 +1,13 @@
+package Practic_clas;
+
+import java.sql.Driver;
+
+public class GitHubGonder {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hey gidi gunler");
+
+
+    }
+}
